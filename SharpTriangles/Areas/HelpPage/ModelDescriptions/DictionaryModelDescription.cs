4ff3daa534368d6bd790acb74b99bc914fb3a7b1
Shein Htike <chocolateshein.htike@gmail.com>
2019-09-23 01:23:31 -0400

    initial commit

---
+++ SharpTriangles/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SharpTriangles.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SharpTriangles/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SharpTriangles.WebApiApplication" Language="C#" %>
