4ee748b622082d73ad9caa8e00dd243ffd71cfe6
Shein Htike <chocolateshein.htike@gmail.com>
2019-09-23 04:56:11 -0400

    API completed

---
+++ SharpTriangles/Controllers/TrianglesController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using SharpTriangles.Models;
+
+namespace SharpTriangles.Controllers
+{
+    public class TrianglesController : ApiController
+    {
+        // GET api/triangles/5
+        public Point[] Get(string id)
+        {
+            char letter = id[0];
+            int col;
+            if (!char.IsLetter(letter) || !int.TryParse(id.Substring(1), out col))
+                return null;
+            col -= 1;
+            if (col < 0)
+                return null;
+            int row = char.ToLower(letter) - 'a';
+            Point start = new Point(y:row * 10,x: (col /2) * 10);
+            Point second = start;
+            if (col % 2 == 0)
+                second.Y += 10;
+            else
+                second.X += 10;
+            Point third = new Point(y:start.Y + 10,x:start.X + 10);
+            return new[] {start, second, third};
+        }
+    }
+}
---
+++ SharpTriangles/Models/Point.cs
@@ -0,0 +1,13 @@
+namespace SharpTriangles.Models
+{
+    public struct Point
+    {
+        public int Y, X;
+
+        public Point(int y, int x)
+        {
+            Y = y;
+            X = x;
+        }
+    }
+}
